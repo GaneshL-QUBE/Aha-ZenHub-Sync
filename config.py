e9a612c909fa30abff60321a6d9f06e95fd37248
GaneshL <ganeshl@C-LM-0850.local>
2019-08-16 17:52:46 +0530

    Initial Config changes to run QubeWire Aha Sync

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 ## Sample Config
-Zenhub_repo_Id='143267090'
-product_id='6619133017241169673'
+Zenhub_repo_Id='142958202'
+product_id='6621690727465297561'
 product_ref='QS' #AHA
-repo_name='Realimage/qube-slate'
-ndurance_key='e9faca67-1495-4104-9c69-31c5d6772535'
+repo_name='Realimage/qube-wire'
+ndurance_key='AIzaSyBZWfFyRfWhmBwZS-vLglhPEb7YSlVSww0'
+Endurance_Source='https://zenhub-aha-integration.firebaseio.com/'
 update_release_dates=False
 Track_due_date=True
 features_source_of_release_date='github'
